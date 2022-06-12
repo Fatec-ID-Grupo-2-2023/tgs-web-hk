a4ab0e87d2f396c4dacfd93bee3e106e34bc73e0
Caique <caiquepatelliescapeline@gmail.com>
2022-06-12 15:25:02 -0300

    v0.6.2
    update dentists function

--- dentist-edit.php
+++ dentist-edit.php
@@ -88,19 +88,6 @@
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a>
-                                <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
@@ -113,6 +100,14 @@
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
+
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                            <li class="breadcrumb-item"><a href="dentist-list.php">Dentistas</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Atualizar Dentista</li>
+                        </ol>
+                    </nav>
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
@@ -164,13 +159,9 @@
                                 <label for="expertise" class="form-label">Especialidade </label>
                                 <input type="text" class="form-control" id="expertise" name="expertise" value="<?= $expertise ?>">
                             </div>
-                            <div class="mb-3 col-12 col-md-12">
-                                <label for="password" class="form-label">Senha</label>
-                                <input type="password" class="form-control" id="password" name="password">
-                            </div>
 
                             <div class="col-12 btn-toolbar flex-row-reverse">
-                                <button type="submit" class="btn btn-primary" name="updateDentist">Cadastrar</button>
+                                <button type="submit" class="btn btn-primary" name="updateDentist">Atualizar</button>
                             </div>
                         </form>
 
